482c2f502de7579e71991689c2c1a347cf59d812
Raziqkhan07 <98086591+Raziqkhan07@users.noreply.github.com>
2022-02-22 16:22:54 +0530

    Update Count_particular_world.c

--- Count_particular_world.c
+++ Count_particular_world.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char*argv[])
+int main()
 {
 int num =0;
 char word[200];
@@ -16,10 +16,10 @@ if (in_file == NULL)
 }
 printf("Enter the word You Want To Know: \t");
 scanf("%s",word);
-while(!feof(in_file))//this loop searches the for the current word
+while(!feof(in_file))
 {
     fscanf(in_file,"%s",string);
-    if(!strcmp(string,word))//if match found increment num
+    if(!strcmp(string,word))
     num++;
 }
 printf("we found the word %s in the file %d times\n",word,num );
